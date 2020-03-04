9c8316e8df190b0d3b42beb7f3158ac7e98e7db9
MzBudi <ronibudianto361@gmail.com>
2020-03-04 21:22:30 +0700

    commit fix aws 1

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const routerNav = require('./src');
-const morgan = require('morgan');
-
+const bodyParser = require("body-parser");
+const routerNav = require("./src");
+const morgan = require("morgan");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true,
-}))
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-app.use(express.static('assets/'))
+app.use(express.static("assets/"));
 
-const server = app.listen(3001, "127.0.0.1", function () {
-    const host = server.address().address
-    const port = server.address().port
+const server = app.listen(
+  process.env.APP_PORT,
+  process.env.DB_PORT,
+  function() {
+    const host = server.address().address;
+    const port = server.address().port;
     console.log("Listening on Host : ", host, " Port : ", port);
-})
+  }
+);
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    if (req.method === "OPTIONS") {
-        res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET")
-        return res.status(200).json({});
-    }
-    next();
-})
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use(morgan("dev"));
 
-
-app.use('/', routerNav);
+app.use("/", routerNav);
